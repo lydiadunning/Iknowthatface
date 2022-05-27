843825485de88e80b659b5f0c01f0c24c07ce3b6
Lydia Dunning <lydia.dunning@gmail.com>
2022-05-26 18:42:02 -0700

    Fix- prioritize viewed media.

--- viewed.py
+++ viewed.py
@@ -8,9 +8,9 @@ class Viewed:
 
     def add_new(self, medium, work_id):
         if medium == 'movie' and work_id not in self.movies:
-            self.movies.append(work_id)
+            self.movies.append(int(work_id))
         if medium == 'tv' and work_id not in self.tv:
-            self.tv.append(work_id)
+            self.tv.append(int(work_id))
 
     def remove_work(self, medium, work_id):
         if medium == 'movie' and work_id in self.movies:
@@ -21,22 +21,12 @@ class Viewed:
     def prioritize(self, medium, work_list):
         print(f'prioritizing {medium}')
         if medium == 'movie':
-            print(self.movies)
-            print(work_list)
             part_1 = [work for work in work_list if work['id'] in self.movies]
             part_2 = [work for work in work_list if work['id'] not in self.movies]
-            print(part_1)
-            print(part_2)
             return part_1 + part_2
-            # return [work for work in work_list if work['id'] in self.movies] + [work for work in work_list if work['id'] not in self.movies]
         if medium == 'tv':
-            print(self.tv)
-            # return [work for work in work_list if work['id'] in self.tv] + [work for work in work_list if work['id'] not in self.tv]
-            print([work['id'] for work in work_list])
             part_1 = [work for work in work_list if work['id'] in self.tv]
             part_2 = [work for work in work_list if work['id'] not in self.tv]
-            print(part_1)
-            print(part_2)
             return part_1 + part_2
 
 
